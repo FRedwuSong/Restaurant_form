d96633459bff9ac708a22f94381c27f0a93fd42d
wusunghan   <diadora1166@gmail.com>
2017-12-01 23:49:56 +0800

    add category select option in restaurant shared form

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -45,9 +45,10 @@ class Admin::RestaurantsController < ApplicationController
   # def edit
   #
   # end
+
   private
   def restaurant_params
-    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image, :category_id)
   end
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
