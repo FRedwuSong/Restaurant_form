056f78be0fe3d72e4e9efc61947f822ad5627387
wusunghan   <diadora1166@gmail.com>
2017-12-23 00:22:00 +0800

    setup route for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
+  resources :users, only: [:index,:show,:update]
+
   resources :categories, only: :show
   root "restaurants#index"
   namespace :admin do
