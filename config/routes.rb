fa619a6a327905ba7c3667f0f016c1f089a94660
wusunghan   <diadora1166@gmail.com>
2017-12-31 20:57:20 +0800

    setup routes for feeds and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
-  end
 
+    collection do
+      #瀏覽所有餐廳最新動態
+      #GET restaurants/feeds
+      get :feeds
+    end
+
+    member do
+      #瀏覽個別餐廳的 Dashboard
+      #GET restaurants/:id/dashboard
+      get :dashboard
+    end
+  end
   resources :users, only: [:show, :edit, :update]
 
   resources :categories, only: :show
