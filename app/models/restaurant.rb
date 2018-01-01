644a02c1f95e83bb655570e2ca2305c3b555baec
wusunghan   <diadora1166@gmail.com>
2018-01-01 22:39:07 +0800

    create favorite model & setup relation among user, restaurant and like

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -13,7 +13,18 @@ class Restaurant < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
 
+  # 「餐廳被很多使用者like」的多對多關聯
+  # 將關聯名稱自訂為 :likeded_users
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+
+
   def is_favorited?(user)
     self.favorited_users.include?(user)
+  end
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
   end
 end
