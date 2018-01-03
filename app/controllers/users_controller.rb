b593eb405289d43b33a2174b103ada73da0b0b19
wusunghan   <diadora1166@gmail.com>
2018-01-03 23:30:51 +0800

    add user index page with follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     @user.update(user_params)
     redirect_to user_path(@user)
   end
+  def index
+    @users = User.all
+  end
+
   private
   def set_user
      @user = User.find(params[:id])
