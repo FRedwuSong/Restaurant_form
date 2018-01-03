41ba1a690c1da4423bd53bd9d1b0c10ae5cddd37
wusunghan   <diadora1166@gmail.com>
2018-01-03 23:49:09 +0800

    更正belonga_to得對象

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,9 +1,9 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
-  
+
   # 「使用者追蹤使用者」的 self-referential relationships 設定
   # 由於 :following 指向 User Model, Rails 無法自動推論
   # 使用 :class_name 告知對應的 Model 名稱
-  belongs_to :users
+  belongs_to :user
   belongs_to :following, class_name: "User"
 end
