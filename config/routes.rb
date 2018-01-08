b7a55d8682775f44e29fb693fb9f7feec0679d5e
wusunghan   <diadora1166@gmail.com>
2018-01-08 21:35:40 +0800

    設定美食達人專頁與好友 / 取消好友的路由

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:index, :show, :edit, :update]
   resources :followships, only: [:create, :destroy]
-
+  resources :friendships, only: [:create, :destroy]
   resources :categories, only: :show
   root "restaurants#index"
   namespace :admin do
